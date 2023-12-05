d30bbc183a91813a6bd1b777dd8f85cf07c8b204
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-12-05 11:06:58 +0000

    Tensorflow install and adding public styling

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 8080;
 // sendFile will go here
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-})
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
