df366c4fc6218779980c0c2340f143d437354b9a
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-03-14 12:55:17 +0000

    Initial version of cake app

--- cake-app/src/routes/layout.tsx
+++ cake-app/src/routes/layout.tsx
@@ -2,8 +2,8 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "../components/starter/header/header";
-import Footer from "../components/starter/footer/footer";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 
 import styles from "./styles.css?inline";
 
@@ -17,12 +17,6 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
     maxAge: 5,
   });
 };
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
 
 export default component$(() => {
   useStyles$(styles);
