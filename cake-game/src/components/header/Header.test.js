e6841c04d757e9b6ffbfd8c476796fd9bad2165a
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-04-19 11:07:04 +0100

    Adding header component tests

--- cake-game/src/components/header/Header.test.js
+++ cake-game/src/components/header/Header.test.js
@@ -6,4 +6,6 @@ test.use({ viewport: { width: 500, height: 500 } });
 test('should work', async ({ mount }) => {
   const component = await mount(<Header />);
   await expect(component).toContainText('Play');
+  await expect(component).toContainText('Rules');
+  await expect(component).toContainText('GitHub');
 });
