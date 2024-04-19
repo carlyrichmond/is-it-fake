36c15a0c5fb3e66a5ae935e3703c0e758a287bc6
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-04-19 11:02:45 +0100

    Porting hero tests from Qwik

--- cake-game/src/components/hero/Hero.test.js
+++ cake-game/src/components/hero/Hero.test.js
@@ -3,7 +3,12 @@ import Hero from './Hero';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('should work', async ({ mount }) => {
+test(`[Hero Component]: Should render title`, async ({ mount }) => {
   const component = await mount(<Hero />);
-  await expect(component).toContainText('Is it (F)ake?!');
+  expect(await component.innerHTML()).toContain('Is it <span class="highlight">(F)ake</span>?!');
+});
+
+test(`[Hero Component]: Should render call to action paragraph`, async ({ mount }) => {
+  const component = await mount(<Hero />);
+  expect(await component.innerHTML()).toContain('Beat the models and find the cake!');
 });
